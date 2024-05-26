d888eccc02ce2c0bffb097f8f61e7c1d87f89c17
Abdul <aabdulmajeed.isa@gmail.com>
2024-05-26 07:11:19 +0100

    update navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,10 +1,12 @@
 "use client"
 import React from "react";
-import {Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button} from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, Button } from "@nextui-org/react";
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 
 export default function Nav() {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const pathname = usePathname()
 
   const menuItems = [
     "Profile",
@@ -14,36 +16,50 @@ export default function Nav() {
   ];
 
   return (
-    <Navbar shouldHideOnScroll onMenuOpenChange={setIsMenuOpen}>
+    <Navbar
+      shouldHideOnScroll
+      onMenuOpenChange={setIsMenuOpen}
+      classNames={{
+        item: [
+          "flex",
+          "relative",
+          "h-full",
+          "items-center",
+          "data-[active=true]:after:content-['']",
+          "data-[active=true]:after:absolute",
+          "data-[active=true]:after:bottom-0",
+          "data-[active=true]:after:left-0",
+          "data-[active=true]:after:right-0",
+          "data-[active=true]:after:h-[2px]",
+          "data-[active=true]:after:rounded-[2px]",
+          "data-[active=true]:after:bg-primary",
+        ],
+      }}
+    >
       <NavbarContent>
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
           className="sm:hidden"
         />
         <NavbarBrand>
-        <Link color="foreground" href="/">
-          <Image src={'/logo/layman_ai_logo_wt.png'} 
-            alt='app logo'
-            width={120}
-            height={120}
-          />
-        </Link>
+          <Link color="foreground" href="/">
+            <Image src={'/logo/layman_ai_logo_wt.png'}
+              alt='app logo'
+              width={120}
+              height={120}
+            />
+          </Link>
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarItem>
-          <Link color="foreground" href="/">
+        <NavbarItem isActive={pathname === '/' ? true : false}>
+          <Link color={`${pathname === '/' ? 'primary' : 'foreground'}`} href="/">
             Home
           </Link>
         </NavbarItem>
-        <NavbarItem isActive>
-          <Link href="#" aria-current="page">
-            Customers
-          </Link>
-        </NavbarItem>
-        <NavbarItem>
-          <Link color="foreground" href="/demo">
+        <NavbarItem  isActive={pathname === '/demo' ? true : false}>
+          <Link color={`${pathname === '/demo' ? 'primary' : 'foreground'}`} href="/demo">
             Demo
           </Link>
         </NavbarItem>
