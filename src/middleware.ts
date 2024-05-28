a8313144d3c558fd9d213fb3f84762cdec8ab581
Abdul <aabdulmajeed.isa@gmail.com>
2024-05-28 15:18:41 +0100

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,33 @@
-export { default } from 'next-auth/middleware'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+
+const protectedPaths = ['/app'];
+const publicPaths = ['/demo'];
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  const isAuthPage = publicPaths.includes(req.nextUrl.pathname);
+  const isProtectedPage = protectedPaths.includes(req.nextUrl.pathname);
+
+  if (token) {
+    if (isAuthPage) {
+      const url = req.nextUrl.clone();
+      url.pathname = '/app';
+      return NextResponse.redirect(url);
+    }
+  } else {
+    if (isProtectedPage) {
+      const url = req.nextUrl.clone();
+      url.pathname = '/login';
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-    matcher: ["/app"]
-}
+  matcher: ['/demo', '/app'],
+};
