e1683b43630c18c6e2f59ee3d738b2e589c8453c
Abdul <aabdulmajeed.isa@gmail.com>
2024-05-29 12:41:47 +0100

    Add feature component

---
+++ src/components/Feature.tsx
@@ -0,0 +1,74 @@
+const Feature: React.FC = () => {
+  return (
+    <section className="py-12 bg-white sm:py-16 lg:py-20">
+      <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+        <div className="text-center">
+          <h2 className="text-3xl font-bold leading-tight text-gray-900 sm:text-4xl xl:text-5xl font-pj">Discover the Power of Layman AI</h2>
+          <p className="mt-4 text-base leading-7 text-gray-600 sm:mt-8 font-pj">Explore the key features that make text simplification effortless</p>
+        </div>
+
+        <div className="grid grid-cols-1 mt-10 text-center sm:mt-16 sm:grid-cols-2 sm:gap-x-12 gap-y-12 md:grid-cols-3 md:gap-0 xl:mt-24">
+          <div className="md:p-8 lg:p-14">
+
+            <svg className="mx-auto text-primary-200" width="70" height="70" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="currentColor">
+              <path fillRule="evenodd" d="M4.848 2.771A49.144 49.144 0 0 1 12 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 0 1-3.476.383.39.39 0 0 0-.297.17l-2.755 4.133a.75.75 0 0 1-1.248 0l-2.755-4.133a.39.39 0 0 0-.297-.17 48.9 48.9 0 0 1-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97ZM6.75 8.25a.75.75 0 0 1 .75-.75h9a.75.75 0 0 1 0 1.5h-9a.75.75 0 0 1-.75-.75Zm.75 2.25a.75.75 0 0 0 0 1.5H12a.75.75 0 0 0 0-1.5H7.5Z" clipRule="evenodd" />
+            </svg>
+
+            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">Instant Text Simplification</h3>
+            <p className="mt-5 text-base text-gray-600 font-pj">Quickly transform complex text into easy-to-understand language with just a click.</p>
+          </div>
+
+          <div className="md:p-8 lg:p-14 md:border-l md:border-gray-200">
+            <svg className="mx-auto text-primary-200" width="70" height="70" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path fillRule="evenodd" d="M2.25 5.25a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3V15a3 3 0 0 1-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 0 1-.53 1.28h-9a.75.75 0 0 1-.53-1.28l.621-.622a2.25 2.25 0 0 0 .659-1.59V18h-3a3 3 0 0 1-3-3V5.25Zm1.5 0v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5Z" clipRule="evenodd" />
+            </svg>
+
+            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">User-Friendly Interface</h3>
+            <p className="mt-5 text-base text-gray-600 font-pj">Navigate seamlessly through a clean, intuitive design for a smooth user experience.</p>
+          </div>
+
+          <div className="md:p-8 lg:p-14 md:border-l md:border-gray-200">
+            <svg className="mx-auto text-primary-200" width="70" height="70" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path d="M16.5 7.5h-9v9h9v-9Z" />
+              <path fillRule="evenodd" d="M8.25 2.25A.75.75 0 0 1 9 3v.75h2.25V3a.75.75 0 0 1 1.5 0v.75H15V3a.75.75 0 0 1 1.5 0v.75h.75a3 3 0 0 1 3 3v.75H21A.75.75 0 0 1 21 9h-.75v2.25H21a.75.75 0 0 1 0 1.5h-.75V15H21a.75.75 0 0 1 0 1.5h-.75v.75a3 3 0 0 1-3 3h-.75V21a.75.75 0 0 1-1.5 0v-.75h-2.25V21a.75.75 0 0 1-1.5 0v-.75H9V21a.75.75 0 0 1-1.5 0v-.75h-.75a3 3 0 0 1-3-3v-.75H3A.75.75 0 0 1 3 15h.75v-2.25H3a.75.75 0 0 1 0-1.5h.75V9H3a.75.75 0 0 1 0-1.5h.75v-.75a3 3 0 0 1 3-3h.75V3a.75.75 0 0 1 .75-.75ZM6 6.75A.75.75 0 0 1 6.75 6h10.5a.75.75 0 0 1 .75.75v10.5a.75.75 0 0 1-.75.75H6.75a.75.75 0 0 1-.75-.75V6.75Z" clipRule="evenodd" />
+            </svg>
+
+
+            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">AI-Powered Accuracy</h3>
+            <p className="mt-5 text-base text-gray-600 font-pj">Utilize advanced AI models from HuggingFace for precise and reliable text simplification.</p>
+          </div>
+
+          <div className="md:p-8 lg:p-14 md:border-t md:border-gray-200">
+            <svg className="mx-auto text-primary-200" width="70" height="70" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path fillRule="evenodd" d="M2.25 6a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V6Zm3.97.97a.75.75 0 0 1 1.06 0l2.25 2.25a.75.75 0 0 1 0 1.06l-2.25 2.25a.75.75 0 0 1-1.06-1.06l1.72-1.72-1.72-1.72a.75.75 0 0 1 0-1.06Zm4.28 4.28a.75.75 0 0 0 0 1.5h3a.75.75 0 0 0 0-1.5h-3Z" clipRule="evenodd" />
+            </svg>
+
+            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">Free Demo Available</h3>
+            <p className="mt-5 text-base text-gray-600 font-pj">Try our service for free with limited usage and experience the benefits firsthand.</p>
+          </div>
+
+          <div className="md:p-8 lg:p-14 md:border-l md:border-gray-200 md:border-t">
+            <svg className="mx-auto text-primary-200" width="70" height="70" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path fillRule="evenodd" d="M12.516 2.17a.75.75 0 0 0-1.032 0 11.209 11.209 0 0 1-7.877 3.08.75.75 0 0 0-.722.515A12.74 12.74 0 0 0 2.25 9.75c0 5.942 4.064 10.933 9.563 12.348a.749.749 0 0 0 .374 0c5.499-1.415 9.563-6.406 9.563-12.348 0-1.39-.223-2.73-.635-3.985a.75.75 0 0 0-.722-.516l-.143.001c-2.996 0-5.717-1.17-7.734-3.08Zm3.094 8.016a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z" clipRule="evenodd" />
+            </svg>
+
+            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">Secure User Registration</h3>
+            <p className="mt-5 text-base text-gray-600 font-pj">Sign up with confidence, knowing your data is protected with our secure authentication system.</p>
+          </div>
+
+          <div className="md:p-8 lg:p-14 md:border-l md:border-gray-200 md:border-t">
+            <svg className="mx-auto text-primary-200" width="70" height="70" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+              <path fillRule="evenodd" d="M2.25 13.5a8.25 8.25 0 0 1 8.25-8.25.75.75 0 0 1 .75.75v6.75H18a.75.75 0 0 1 .75.75 8.25 8.25 0 0 1-16.5 0Z" clipRule="evenodd" />
+              <path fillRule="evenodd" d="M12.75 3a.75.75 0 0 1 .75-.75 8.25 8.25 0 0 1 8.25 8.25.75.75 0 0 1-.75.75h-7.5a.75.75 0 0 1-.75-.75V3Z" clipRule="evenodd" />
+            </svg>
+
+            <h3 className="mt-12 text-xl font-bold text-gray-900 font-pj">Versatile Usage</h3>
+            <p className="mt-5 text-base text-gray-600 font-pj">Ideal for students, professionals, and anyone who needs to simplify complex information.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  )
+}
+export default Feature;
