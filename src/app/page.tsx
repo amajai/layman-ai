7777c0082e4773bf6c3c2ed6c9127d32dc8cab4c
Abdul <aabdulmajeed.isa@gmail.com>
2024-05-29 12:43:37 +0100

    Add components to front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
+import CTA from '@/components/CTA';
+import Feature from '@/components/Feature';
+import Footer from '@/components/Footer';
 import Hero from '@/components/Hero'
+import HowItWorks from '@/components/HowItWorks';
+import Overview from '@/components/Overview';
+import React from 'react';
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <>
-      <Hero></Hero> 
+      <Hero/>
+      <HowItWorks/>
+      <Feature/>
+      <Overview/>
+      <CTA/>
+      <Footer/>
     </>
   );
 }
+
+export default App;
